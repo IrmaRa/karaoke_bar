7c35abc75b0eb62644ca1135c8b87feab7fa2632
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-04-22 11:52:37 +0100

    Add guest favourite song property

--- guest.rb
+++ guest.rb
@@ -2,8 +2,13 @@ class Guest
 
   attr_reader :name
 
-  def initialize(name)
+  attr_accessor :money, :favourite_song
+
+  def initialize(name, money, favourite_song)
     @name = name
+    @money = money
+    @favourite_song = favourite_song
+
   end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,8 +7,18 @@ require_relative('../guest')
 class TestGuest < MiniTest::Test
 
   def test_guest_has_a_name
-    guest = Guest.new("Ben")
+    guest = Guest.new("Ben", 50, "Tennis Court")
     assert_equal("Ben", guest.name)
+  end
+
+  def test_guest_has_money
+    guest = Guest.new("Ben", 50, "Come With Me")
+    assert_equal(50, guest.money)
+  end
+
+  def test_guest_has_favourite_song
+    guest = Guest.new("Ben", 50, "Come With Me")
+    assert_equal("Come With Me", guest.favourite_song)
   end
 
 end
