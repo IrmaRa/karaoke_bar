24e9896f40cd6b48fca6a6ac81e0e361897eac3a
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-04-23 14:22:40 +0100

    Add check name and check in functions

--- runner.rb
+++ runner.rb
@@ -2,20 +2,17 @@ require_relative('./guest')
 require_relative('./song')
 require_relative('./room')
 require_relative('./viewer')
+require_relative('./karaoke_bar')
 
 class Runner
 
   def initialize(viewer)
     @viewer = viewer
 
-    guest_1_name = @viewer.get_guest_name(1)
-    guest_2_name = @viewer.get_guest_name(2)
 
-    guest1 = Guest.new(guest_1_name, 20, "Video Games
+    @guest1 = Guest.new(@viewer.get_guest_name, @viewer.check_guest_money, "Video Games
       ")
-    guest2 = Guest.new(guest_2_name, 8, "Cape Town")
 
-    guests = [guest1, guest2]
 
     song1 = Song.new("Video Games")
     song2 = Song.new("Blue Jeans")
@@ -24,10 +21,16 @@ class Runner
 
     songs = [song1, song2, song3, song4]
 
-    room_regular = Room.new("Regular", 10, guests, songs)
+    @room_regular = Room.new("Regular", 10, [], songs)
 
   end
+
+  def run()
+    @viewer.add_guest(@room_regular.check_in(@guest1.name))
+  end
+
 
 end
 
 karaoke = Runner.new(Viewer.new)
+karaoke.run()
--- viewer.rb
+++ viewer.rb
@@ -1,9 +1,22 @@
 class Viewer
 
-  def get_guest_name(guest_number)
-    puts "Hello! Please enter #{guest_number} name:"
+  def get_guest_name()
+    puts "Hello! Please enter guest's name:"
     input = gets.chomp.capitalize
-    puts "#{input} you were successfully checked in!"
+  end
+
+  def add_guest(guest_name)
+    puts "#{guest_name} was successfully checked in!"
+  end
+
+  def check_guest_money()
+    puts "How much money does guest have?"
+    input = gets.chomp.to_i
+    if input >= 10
+      puts "Great! Guest can come in!"
+    else
+      puts "It's guest's lucky day! Guest can come in!"
+    end
   end
   
 end
